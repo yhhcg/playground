f76c1560c602c0373ab7a344e90b534ab991f48c
余珩 <1281005689@qq.com>
2018-08-14 21:36:31 +0800

    [NormalizedData] Show all tags

--- src/NormalizedData/components/TagList/selectors.js
+++ src/NormalizedData/components/TagList/selectors.js
@@ -4,10 +4,8 @@ import {createSelector} from 'reselect';
 export const tagListSelector = createSelector(
   (state) => state.normalizedData.todoTags,
   (todoTags) => {
-    const tags = Object.entries(todoTags.byId).reduce((preResult, [tagId, tag]) => {
+    return Object.entries(todoTags.byId).reduce((preResult, [tagId, tag]) => {
       return preResult.concat([tag.name]);
     }, []);
-
-    return [...new Set(tags)];
   }
 );
